8aff0f913756bd3d0773720abc44d78ede77fda4
gabatk <gabriela.trybuszek@gmail.com>
2023-02-17 22:28:27 +0100

    JS file added.

---
+++ src/js/main.js
@@ -0,0 +1,5 @@
+const burgerBtn = document.querySelector('.nav-mobile__icon');
+const navMobile = document.querySelector('.nav-mobile');
+const navDesktop = document.querySelector('.nav-desktop');
+
+const handleMobileNav = () => {};
