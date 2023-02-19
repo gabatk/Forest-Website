99b449d80f28174327708b2726bda96e5e089b05
gabatk <gabriela.trybuszek@gmail.com>
2023-02-19 23:06:38 +0100

    Mobile nav function completed.

--- src/js/main.js
+++ src/js/main.js
@@ -2,30 +2,22 @@ const burgerBtn = document.querySelector('.nav-mobile__icon');
 const navMobile = document.querySelector('.nav-mobile');
 const navLinks = document.querySelectorAll('.nav-mobile__link');
 const navDesktop = document.querySelector('.nav-desktop');
+const contentPart = document.querySelector('.content');
 
 const handleMobileNav = () => {
 	navMobile.style.display = 'block';
-   
-	// handleLinks()   
-    };
-
-const handleLinks = () => {
-    navLinks.forEach(link => {
-        link.addEventListener('click', hideMobileNav);
-               	});
+    handleLinks()
 };
 
-const hideMobileNav = () => { 
+const handleLinks = () => {
+	navLinks.forEach(link => {
+		link.addEventListener('click', hideMobileNav);
+	});
+};
+
+const hideMobileNav = () => {
 	navMobile.style.display = 'none';
 };
 
-const clickedElement = (e) => {
-if (!navMobile.contains(e.target) || !burgerBtn.contains(e.target)) {
-    navMobile.style.display = 'none';
-}
-}
-
 burgerBtn.addEventListener('click', handleMobileNav);
-// burgerBtn.addEventListener('click', handleLinks);
-handleLinks()
-document.addEventListener('click', clickedElement)
+contentPart.addEventListener('click', hideMobileNav);
