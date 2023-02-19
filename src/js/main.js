3fcfc47c32ede7501c6883e50cd3d60ceb29b8a0
gabatk <gabriela.trybuszek@gmail.com>
2023-02-19 23:39:55 +0100

    Made some new changes.

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,6 @@
 const burgerBtn = document.querySelector('.nav-mobile__icon');
 const navMobile = document.querySelector('.nav-mobile');
 const navLinks = document.querySelectorAll('.nav-mobile__link');
-const navDesktop = document.querySelector('.nav-desktop');
 const contentPart = document.querySelector('.content');
 
 const handleMobileNav = () => {
