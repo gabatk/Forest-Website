4ab669533af4d1fc895f84905ce23926895f8772
gabatk <gabriela.trybuszek@gmail.com>
2023-02-19 21:46:03 +0100

    Nav function in progress.

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,22 @@
 const burgerBtn = document.querySelector('.nav-mobile__icon');
 const navMobile = document.querySelector('.nav-mobile');
+const navLinks = document.querySelectorAll('.nav-mobile__link');
+
 const navDesktop = document.querySelector('.nav-desktop');
 
-const handleMobileNav = () => {};
+const handleMobileNav = () => {
+	navMobile.style.display = 'block';
+	handleLinks();
+};
+
+const handleLinks = () => {
+	navLinks.forEach(link => {
+		link.addEventListener('click', hideMobileNav);
+	});
+};
+
+const hideMobileNav = () => {
+	navMobile.style.display = 'none';
+};
+
+burgerBtn.addEventListener('click', handleMobileNav);
